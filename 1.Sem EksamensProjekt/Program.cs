083a3b27ce3fecbf0a7b1e8221bd43f61a759c31
Jonaslk727 <joloknu@gmail.com>
2025-05-13 06:44:56 +0200

    Add project files.

---
+++ 1.Sem EksamensProjekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.Sem_EksamensProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
