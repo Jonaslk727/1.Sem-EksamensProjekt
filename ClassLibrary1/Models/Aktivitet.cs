ab338cf36183216c50c186bd95ee9811aa4f6114
Andershgras <andershgras@gmail.com>
2025-05-14 12:51:23 +0200

    Aktivitet class: Lagt Properties i en region Slettet Default Class1

--- ClassLibrary1/Models/Aktivitet.cs
+++ ClassLibrary1/Models/Aktivitet.cs
@@ -8,12 +8,15 @@ namespace ClassLibrary1.Models
 {
     public class Aktivitet
     {
+        #region Properties
         public int AktivitetID { get; set; }
         private static int nextId = 0;
         public string Title { get; set; }
         public DateTime StartTid { get; set; }
         public DateTime SlutTid { get; set; }
         public string Beskrivelse { get; set; }
+        #endregion
+
         //public List<Kunde> Tilmeldte { get; set; } = new List<Kunde>();
 
         public Aktivitet(string title, DateTime start, DateTime slut, string beskrivelse)
