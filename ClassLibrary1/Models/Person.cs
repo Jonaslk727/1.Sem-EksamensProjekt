4fbc6334c40da8bc59a07a588fce67cd63f8a9da
MaxWolmer <mwtj1000@gmail.com>
2025-05-14 12:24:15 +0200

    Har lavet skabt en person klasse og en kunderepo

---
+++ ClassLibrary1/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Models
+{
+    internal class Person
+    {
+        public string Navn { get; set; }
+        public string Email { get; set; }
+        public int Mobil { get; set; }
+
+
+
+        public Person(string navn, string email, int mobil)
+        {
+            Navn = navn;
+            Email = email;
+            Mobil = mobil;
+        }
+    }
+}
