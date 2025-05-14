0b181b4d041697177ed37d5827dd8ee4ad6c65a2
MaxWolmer <mwtj1000@gmail.com>
2025-05-14 12:11:18 +0200

    Har lavet kundeklassen

--- ClassLibrary1/Models/Kunde.cs
+++ ClassLibrary1/Models/Kunde.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1.Models;
 
 namespace ClassLibrary1.Models
 {
     internal class Kunde
-    {   public int Id { get; set; } 
+    {
+        public int KundeId { get; set; }
         public DateTime DateOfBirth { get; set; }
         public DateTime RegistrationDate { get; set; }
         public bool IsPremiumMember { get; set; }
-    }
 
-    {
-        public override string ToString();
 
+
+        public Kunde(int kundeId, DateTime dateOfBirth, DateTime registrationDate, bool isPremiumMember)
+        {
+            KundeId = kundeId;
+            DateOfBirth = dateOfBirth;
+            RegistrationDate = registrationDate;
+            IsPremiumMember = isPremiumMember;
+        }
     }
-    var kunde = new Kunde { Id = "1", Email = "Andersjo@gmail.com", Mobile = 20102319}
 }
