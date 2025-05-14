40dc9bf524460b5b66b9bf5e7b57c277c21332e5
Jonaslk727 <joloknu@gmail.com>
2025-05-14 12:25:17 +0200

    lavet Dyr classen + DyreLog classen

---
+++ ClassLibrary1/Services/DyreLog.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Services
+{
+    public class DyreLog: ICRUD
+    {
+        List< string> MedicinskLog = new List<string>();
+        List<string> BesøgssLog = new List<string>();
+
+        
+        public void Read()
+        {
+            // Implementer logik til at læse loggen
+        }
+        public void Update()
+        {
+            // Implementer logik til at opdatere loggen
+        }
+        public void Delete()
+        {
+            // Implementer logik til at slette loggen
+        }
+    }
+}
