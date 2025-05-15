3cbd1177fb5e0d0f88bc95d8759ea68730ec1f8f
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-05-15 09:49:36 +0200

    Modifications in Medarbejder

--- ClassLibrary1/Services/MedarbejderRepo.cs
+++ ClassLibrary1/Services/MedarbejderRepo.cs
@@ -1,12 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ClassLibrary1.Models;
+using ClassLibrary1.Interfaces;
+using System.Security.Cryptography.X509Certificates;
 
-namespace ClassLibrary1.Services
+namespace ClassLibrary1.Repositories
 {
-    internal class MedarbejderRepo
+    public class MedarbejderRepo : IMedarbejder
     {
+        private List<Medarbejder> medarbejdere = new List<Medarbejder>();
+
+        public void TilføjMedarbejder(Medarbejder medarbejder)
+        {
+            medarbejdere.Add(medarbejder);
+        }
+
+        public void VisMedarbejder()
+        {
+            List<Medarbejder> medarbejdere = HentAlleMedarbejdere();
+
+            if (medarbejdere.Count == 0)
+            {
+                Console.WriteLine("Ingen medarbejdere fundet.");
+                return;
+            }
+
+            Console.WriteLine("\n=== Liste over medarbejdere ===");
+            foreach (var medarbejder in medarbejdere)
+            {
+                Console.WriteLine(medarbejder);
+            }
+        }
+
+        public bool FindMedarbejder(int medarbejderId)
+        {
+            foreach (Medarbejder medarbejder in medarbejdere)
+            {
+                if (medarbejder.MedarbejderId == medarbejderId)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void OpdaterMedarbejder(int medarbejderId, Medarbejder opdateretMedarbejder)
+        {
+            foreach (Medarbejder medarbejder in medarbejdere)
+            {
+                if (medarbejder.MedarbejderId == medarbejderId)
+                {
+                    medarbejder.Navn = opdateretMedarbejder.Navn;
+                    medarbejder.Afdeling = opdateretMedarbejder.Afdeling;
+                    medarbejder.Stilling = opdateretMedarbejder.Stilling;
+                    medarbejder.Email = opdateretMedarbejder.Email;
+                    medarbejder.Telefonnummer = opdateretMedarbejder.Telefonnummer;
+                    break;
+                }
+            }
+
+        }
+        public void SletMedarbejder(int medarbejderId)
+        {
+            for (int i = 0; i < medarbejdere.Count; i++)
+            {
+                if (medarbejdere[i].MedarbejderId == medarbejderId)
+                {
+                    medarbejdere.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public List<Medarbejder> HentAlleMedarbejdere()
+        {
+            return medarbejdere;
+        }
     }
 }
