98377bdef751583c8330adeba87b26b90925fceb
MaxWolmer <mwtj1000@gmail.com>
2025-05-15 09:28:48 +0200

    Har lavet IKunde klasse og ændringer i Kunde klassen

--- ClassLibrary1/Models/Kunde.cs
+++ ClassLibrary1/Models/Kunde.cs
@@ -15,10 +15,30 @@ namespace ClassLibrary1.Models
         public DateTime DateOfBirth { get; set; }
         public DateTime RegistrationDate { get; set; }
         public bool IsPremiumMember { get; set; }
+        public string Mobil { get; set; } // Added this property to fix the issue
 
-        
+        public void VisInfo()
+        {
+            Console.WriteLine($"Kunde ID: {KundeId}");
+            Console.WriteLine($"Navn: {Navn}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Telefon: {Mobil ?? "N/A"}");
+        }
+
+        public void OpdaterMobil(string nyMobil)
+        {
+            if (!string.IsNullOrEmpty(nyMobil))
+            {
+                Mobil = nyMobil;
+                Console.WriteLine($"Telefon opdateret til: {Mobil}");
+            }
+            else
+            {
+                Console.WriteLine("Ugyldigt telefonnummer!");
+            }
+        }
+
         public Kunde(int kundeId, DateTime dateOfBirth, DateTime registrationDate, bool isPremiumMember)
-           
         {
             KundeId = kundeId;
             DateOfBirth = dateOfBirth;
