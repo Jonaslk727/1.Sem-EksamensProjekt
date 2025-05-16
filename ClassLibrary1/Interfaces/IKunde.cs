b7d4b84ffcfb5c6b17943ff7114e49561b386ad9
MaxWolmer <mwtj1000@gmail.com>
2025-05-16 12:17:25 +0200

    Ikunde lavet

--- ClassLibrary1/Interfaces/IKunde.cs
+++ ClassLibrary1/Interfaces/IKunde.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1.Models;
 
 namespace ClassLibrary1.Interfaces
 {
-    internal class Kunde
+    public interface IKunde
     {
-        
+        void TilføjKunde(Kunde kunde);
+
+        void VisKunde();
+
+        bool FindKunde(int kundeId);
+
+        void OpdaterKunde(int kundeId, Kunde kunde);
+
+        void SletKunde(int kundeId);
+
+        List<Kunde> HentAlleKunder();
     }
 }
