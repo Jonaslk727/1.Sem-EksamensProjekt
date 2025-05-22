eb124ae58b26f101e1f8cf1894ad92a25e8f941d
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-05-22 09:56:25 +0200

    Har tilføjet kommentarer til Besøg-klassen og IMedarbejder-interfacet."

--- ClassLibrary1/Models/Besøg og lægelog/Besøg.cs
+++ ClassLibrary1/Models/Besøg og lægelog/Besøg.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1.Models.Besøg_og_lægelog
 {
+    /// <summary>
+    /// Klasse: Besøg  
+    /// Håndterer registrering af besøg på Roskilde Dyreinternat,  
+    /// inklusiv tidspunkt, besøgende (kunde) og automatisk  
+    /// generering af unikt besøgs-ID.  
+    /// Indeholder en ToString()-metode for let visning af besøg.  
+    /// </summary>
+
+
     public class Besøg
     {
         public DateTime BesøgsTidspunkt { get; set; }
@@ -13,6 +22,8 @@ namespace ClassLibrary1.Models.Besøg_og_lægelog
         private static int _nextId = 1;
         public Kunde Besøger { get; set; }
         public int BesøgsId { get; init; }
+
+        /// Konstruktør for Besøg-klassen, som initialiserer besøgsdata.
 
         public Besøg(DateTime besøgsTidspunkt, Kunde besøger)
         {
